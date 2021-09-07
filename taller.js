330670013e4acd1398497d0975598edcb669409a
Geudiel Efren <38485056+Geudiel1988@users.noreply.github.com>
2021-09-07 16:01:07 -0500

    se agrega style y correcciones

--- taller.js
+++ taller.js
@@ -1,71 +1,68 @@
-function suma(){
-var a = document.getElementById("n_1").value;
-var b = document.getElementById("n_2").value;
+function suma() {
+    let a = document.getElementById("n_1").value;
+    let b = document.getElementById("n_2").value;
 
-var opera=parseInt(a)+parseInt(b);
+    let opera = parseInt(a) + parseInt(b);
 
-var res = document.getElementById("res");
-res.innerHTML="la suma es "+opera ;
+    let res = document.getElementById("res");
+    res.innerHTML = "la suma es " + opera;
 }
 
 
-function edad(){
-var edad = document.getElementById("edad").value;
-var resultado = document.getElementById("resultado");
+function edad() {
+    let edad = document.getElementById("edad").value;
+    let resultado = document.getElementById("resultado");
 
-	if(edad >= 18){
-	
-	resultado.innerHTML="es mayor de edad" 
-	}
-	else{
-	  
-	 resultado.innerHTML =" es menor de edad"
-	}
+    if (edad >= 18) {
+
+        resultado.innerHTML = "es mayor de edad"
+    } else {
+
+        resultado.innerHTML = " es menor de edad"
+    }
 }
 
 
-function mayor(){
-	var n1 = document.getElementById("s1").value;
-	var n2 = document.getElementById("s2").value;
-	var n3 = document.getElementById("s3").value;
-	var re= document.getElementById("re");
-	
-	if(n1>n2 && n1>n3){
-	re.innerHTML="resultado"+n1;
-	}
-	else if(n2>n3 && n2>n1){
-	re.innerHTML="resultado"+n2;
-	}
-	else if(n3>n1 && n3>n2){
-	re.innerHTML="resultado  " +n3;
-	}
-	
-}
-function factorial(){
-	var fac = document.getElementById("fact").value;
-	var resu = document.getElementById("resu");
-	var factorial =1
-	for(var i= 1; i<=fac;i++){
-	factorial= factorial*i;
+function mayor() {
+    let n1 = document.getElementById("s1").value;
+    let n2 = document.getElementById("s2").value;
+    let n3 = document.getElementById("s3").value;
+    let re = document.getElementById("re");
 
-     resu.innerHTML="resultado " +factorial;
-     }
+    if (n1 > n2 && n1 > n3) {
+        re.innerHTML = "resultado" + n1;
+    } else if (n2 > n3 && n2 > n1) {
+        re.innerHTML = "resultado" + n2;
+    } else if (n3 > n1 && n3 > n2) {
+        re.innerHTML = "resultado  " + n3;
+    }
+
 }
 
-function promedio(){
-	var n1 = document.getElementById("nota1").value;
-	var n2 = document.getElementById("nota2").value;
-	var n3 = document.getElementById("nota3").value;
-	var promedio=document.getElementById("prom");
-	
-	var operacion = (parseInt(n1)+parseInt(n2)+parseInt(n3))/3;
-	
-	if(operacion >= 7){
-	
-	promedio.innerHTML="el alumno aprueba " + operacion;
-	}
-	else{
-	promedio.innerHTML="el alumno reprobo " + operacion;
-	}
+function factorial() {
+    let fac = document.getElementById("fact").value;
+    let resu = document.getElementById("resu");
+    let factorial = 1
+    for (let i = 1; i <= fac; i++) {
+        factorial = factorial * i;
+
+        resu.innerHTML = "resultado " + factorial;
+    }
+}
+
+function promedio() {
+    let n1 = document.getElementById("nota1").value;
+    let n2 = document.getElementById("nota2").value;
+    let n3 = document.getElementById("nota3").value;
+    let promedio = document.getElementById("prom");
+
+    let operacion = (parseInt(n1) + parseInt(n2) + parseInt(n3)) / 3;
+
+    if (operacion >= 7) {
+
+        promedio.innerHTML = "el alumno aprueba " + operacion;
+    } else {
+        promedio.innerHTML = "el alumno reprobo " + operacion;
+    }
 
 }
